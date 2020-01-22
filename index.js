43328d4133ff996cc78d8b179fa2e0f556ffaf88
adiamahardika <adiamahardika.work@gmail.com>
2020-01-23 00:06:03 +0700

    Menambahkan removetask ke file index.js dan index.ejs

--- index.js
+++ index.js
@@ -9,16 +9,34 @@ app.use(express.static("public"))
 
 let task = []
 
-app.post("/add", function(req,res) {
+let complete = []
+
+app.post("/addtask", function(req,res) {
     console.log(req.body)
     let newTask = req.body.newtask
-
     task.push(newTask)
     res.redirect("/")
 })
 
+app.post("/removetask", function(req, res) {
+    let completeTask = req.body.check
+
+    if (Array.isArray(completeTask)) {
+        for (var i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i])
+            task.splice(task.indexOf(completeTask[i]), 1)
+        }
+    } else {
+        complete.push(completeTask)
+        task.splice(task.indexOf(completeTask[i]), 1)
+    }
+
+    res.redirect("/")
+    
+})
+
 app.get("/", function(req, res) {
-    res.render("index", {task: task})
+    res.render("index", {task: task, complete : complete})
 }) 
 
 app.listen(1500, function() {
